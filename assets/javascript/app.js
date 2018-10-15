b3b1d3205c6d04cd405773f1af00c126e644bdd7
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-10-15 18:32:47 -0500

    firebase initialized

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyC-CdT3dQA1hQ-hsnusEJkoQMiMx4r6WgY",
+    authDomain: "train-scheduler-ea.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ea.firebaseio.com",
+    projectId: "train-scheduler-ea",
+    storageBucket: "train-scheduler-ea.appspot.com",
+    messagingSenderId: "368394684268"
+  };
+  firebase.initializeApp(config);
+
+  
